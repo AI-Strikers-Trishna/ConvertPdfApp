bf6698f7e61d1809757e38db6cdcffff64b2ba55
Trishna Das <tdas@AIENTERPRISE.COM>
2019-09-12 17:20:22 +0530

    First commit

--- ConvertPdfApp/Controllers/HomeController.cs
+++ ConvertPdfApp/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,6 +29,21 @@ namespace ConvertPdfApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public FileResult Export(string ExportData)
+        {
+            using (MemoryStream stream = new System.IO.MemoryStream())
+            {
+                StringReader reader = new StringReader(ExportData);
+                Document PdfFile = new Document(PageSize.A4);
+                PdfWriter writer = PdfWriter.GetInstance(PdfFile, stream);
+                PdfFile.Open();
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, PdfFile, reader);
+                PdfFile.Close();
+                return File(stream.ToArray(), "application/pdf", "ExportData.pdf");
+            }
         }
     }
 }
